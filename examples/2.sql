df5b5252ff4faee2b1482622934d724555224748
James Hudon <jameshudon@gmail.com>
2013-11-21 13:12:44 -0500

    support transactions

--- examples/2.sql
+++ examples/2.sql
@@ -4,10 +4,17 @@
 
 -- UP
 
+-- the 2 sections are read by psql as sql scripts, so everything is supported
+-- (eg transactions)
 
-alter table foo add name varchar(32);
+BEGIN;
+create table name (name varchar(32) primary key);
+alter table foo add name varchar(32) references name;
+COMMIT;
 
 -- DOWN
+
 alter table foo drop column name;
+drop table name;
 
 
