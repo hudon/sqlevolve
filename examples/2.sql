146578ae549328f5eeb2c3f4e0d13b53e2ef3084
James Hudon <jameshudon@gmail.com>
2013-11-21 18:29:54 -0500

    wrap scripts in an implicit transaction

--- examples/2.sql
+++ examples/2.sql
@@ -1,16 +1,14 @@
 
 -- whitespace and comments are ignored (except for UP and DOWN)
+-- the script is wrapped in a transaction: if one statement fails, the change
+-- fails
 
 
 -- UP
 
--- the 2 sections are read by psql as sql scripts, so everything is supported
--- (eg transactions)
 
-BEGIN;
 create table name (name varchar(32) primary key);
 alter table foo add name varchar(32) references name;
-COMMIT;
 
 -- DOWN
 
